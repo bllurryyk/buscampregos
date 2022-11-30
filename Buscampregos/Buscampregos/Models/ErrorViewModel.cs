52a2afe5711ee4ea6915fe4dfab92df786bda353
bllurryyk <khgds1@hotmail.com>
2022-11-30 17:25:09 -0300

    criando projeto

---
+++ Buscampregos/Buscampregos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Buscampregos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
