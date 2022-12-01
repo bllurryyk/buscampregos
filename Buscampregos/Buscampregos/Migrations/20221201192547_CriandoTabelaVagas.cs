a3b0e196dad06a52b8468d3a41f79c7f00a3dbf6
bllurryyk <khgds1@hotmail.com>
2022-12-01 17:32:06 -0300

    Configurando DB Local e adicionando Migrations

---
+++ Buscampregos/Buscampregos/Migrations/20221201192547_CriandoTabelaVagas.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Buscampregos.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriandoTabelaVagas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vagas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cargo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Empresa = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Local = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TempoPostagem = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Periodo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vagas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vagas");
+        }
+    }
+}
--- Buscampregos/Buscampregos/Program.cs
+++ Buscampregos/Buscampregos/Program.cs
@@ -1,7 +1,14 @@
+using Buscampregos.Content;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<BancoContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DataBase")));
 
 var app = builder.Build();
 
