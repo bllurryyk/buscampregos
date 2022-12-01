33075a7e96d5cf13b746591d9cad454b90464aa2
bllurryyk <khgds1@hotmail.com>
2022-12-01 15:01:47 -0300

    Configurando contexto para DB SQL

---
+++ Buscampregos/Buscampregos/Content/BancoContext.cs
@@ -0,0 +1,14 @@
+﻿using Buscampregos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Buscampregos.Content
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<VagaModel> Vagas { get; set; }
+    }
+}
---
+++ Buscampregos/Buscampregos/Models/VagaModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Buscampregos.Models
+{
+    public class VagaModel
+    {
+        public int Id { get; set; }
+        public string Cargo { get; set; }
+        public string Empresa { get; set; }
+
+        public string Local { get; set; }
+        public string TempoPostagem { get; set; }
+        public string Periodo { get; set; }
+    }
+}
